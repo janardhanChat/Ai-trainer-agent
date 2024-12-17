fd1029f14347ecea4958c863d0fd1ca71fdff2b4
Janardhan <janardhan276.rejoice@gmail.com>
2024-12-17 09:50:51 +0530

    Feat : Add tavus api utils

---
+++ src/api/api/createConversation.js
@@ -0,0 +1,26 @@
+import { TAVUS_API_KEY } from '@/config';
+
+export const createConversation = async () => {
+  try {
+    const response = await fetch('https://tavusapi.com/v2/conversations', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': TAVUS_API_KEY,
+      },
+      body: JSON.stringify({
+        persona_id: 'p9a95912', // Stock Demo Persona
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+};
---
+++ src/config/index.js
@@ -0,0 +1,1 @@
+export const TAVUS_API_KEY = import.meta.env.NEXT_PUBLIC_TAVUS_API_KEY;
