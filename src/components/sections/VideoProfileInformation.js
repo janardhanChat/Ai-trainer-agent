f1b7a5cb1c658abe7fb566803d85a87c706d33c8
Janardhan <janardhan276.rejoice@gmail.com>
2024-12-26 18:29:19 +0530

    chore : Fix build issues

--- src/components/sections/VideoProfileInformation.js
+++ src/components/sections/VideoProfileInformation.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 const DummyProfile = "/images/dummy-profile.svg";
 export default function VideoProfileInformation() {
-  const userInfo = JSON.parse(localStorage.getItem("userInformation"));
+  let userInfo;
+  useEffect(() => {
+    userInfo = JSON.parse(localStorage.getItem("userInformation"));
+  }, []);
   return (
     <button className="py-1.5 px-3 border border-solid border-whiteBorder bg-whiteOpacitybg30 cursor-pointer backdrop-blur-xl	 rounded-full flex items-center gap-2.5 text-lg text-white font-semibold">
       <img src={DummyProfile} alt="DummyProfile" />
