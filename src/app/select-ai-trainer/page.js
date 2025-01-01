3d9a986ead1720d68d4b2a1f56e773a87852d817
Janardhan <janardhan276.rejoice@gmail.com>
2025-01-01 14:28:10 +0530

    Fix: Update heading from 'Select An AI Trainer' to 'Select AI Doctor' for clarity

--- src/app/select-ai-trainer/page.js
+++ src/app/select-ai-trainer/page.js
@@ -47,7 +47,7 @@ export default function page() {
     <div className="bg-page-gradient min-h-screen py-[60px]">
       <div className="max-w-[1780px] px-5 mx-auto">
         <h2 className="text-[28px] text-black font-bold leading-normal mb-[30px]">
-          Select An AI Trainer
+          Select AI Doctor
         </h2>
         <AITrainerSection
           handleOpenmodal={handleOpenmodal}
